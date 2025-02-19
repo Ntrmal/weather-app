4cf79fd2f54ca58cc5279aaf7ba9353c03759a4a
nirmal <nirmalthambi001@gmail.com>
2025-02-19 19:39:20 +0530

    Fixed routing issue for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Favorites from "./components/FavoritePage";
 import About from './About';
-import './app.css'
+import './app.css';
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
-    <Router>
+    <Router basename="/weather-app">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/favorites" element={<Favorites />} />
-        <Route path="/about" element={<About/>}/>
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   );
