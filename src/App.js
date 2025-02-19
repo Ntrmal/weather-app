ef273f6ef8b088c9f559fd38be2a820423416120
nirmal <nirmalthambi001@gmail.com>
2025-02-19 19:54:43 +0530

    Fix signup route with HashRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
